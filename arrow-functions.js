43f95304f197e0846c798c1088a7710b1e002330
Peter Lindblom <peter.lindblom01@gmail.com>
2023-08-26 22:00:32 +0000

    Arrow functions lesson

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,41 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
 
-// Arrow Function With Parameters
+/*function addTwoNumbers(a, b) {
+  return a+b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);*/
 
-// Single Line Arrow Function With Parameters
+// Arrow Function with Parameters
+
+const addTwoNumbers = (a, b) => {
+  return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
+// Single Line Arrow Function With Parameters 
+
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum);
 
 // Implicit Returns
 
+const greeting = message => console.log(message);
+greeting('Hello, John');
+
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+  `<p>
+    This is a multiple string!
+  </p>`
+)
+console.log(returnMultipleLines());
